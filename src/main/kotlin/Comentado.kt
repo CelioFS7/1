06dd9ee34754d20c211a33a235bfce8828daeb20
Celiofs7 <celioferreiramlm@gmail.com>
2021-01-27 09:20:13 -0300

    TEste

---
+++ src/main/kotlin/Comentado.kt
@@ -0,0 +1,233 @@
+
+    //Variável é um mecanismo das linguagens de programação que facilita colocar, remover, acessar e modificar um ou
+    // mais dados na memória do computador.
+
+    /*val
+
+      Essa é a forma mais utilizada e recomenda para declarar uma variável. Com val a variável recebe valor uma única vez,
+    podendo ser utilizada somente para a leitura de valores.Toda variável declarada com val deve ser iniciada com um valor,
+    que não poderá ser modificado. Caso se tente alterar o valor de uma variável declarada com val, um erro de compilação
+    será emitido.Ex.
+    val preco: Float = 2.99f
+    preco = 3.99f
+    Ao tentar modificar o valor de uma variável declarada com val receberemos um erro de compilação com a mensagem Val cannot be reassigned.
+     */
+
+    /* var
+
+    Caso uma variável deva mudar de valor, usamos var em sua declaração,
+    var preco: Float = 2.99f Declaração com var
+    Ao contrário de val, var não requer que a variável seja iniciada com um valor.
+    var preco: Float .Contém um exemplo dessa sintaxe.
+    Embora possamos omitir o valor quando usamos var, o tipo da variável ainda é obrigatório nesse caso.
+    Um erro de compilação é emitido caso tipo e valor estejam ausentes na declaração de uma variável
+    var preco A omissão de tipo e valor gera um erro de compilação.
+    O primeiro erro possui a mensagem This variable must either have a type annotation or be initialized, que diz
+    respeito a ausência de valor e tipo de valor. A segunda mensagem de erro é Variable 'preco' must be initialized,
+    que se refere a ausência de um valor especificamente, porque se um valor está presente o compilador consegue inferir
+    o tipo de uma variável.
+
+     */
+    /*
+    Type              Bit
+
+    : Double          64
+    : Float             32 para usar esse coloca a letra f na frente do numero
+    -------------------------- As de cima números quebrados ou com pontos
+    -------------------------- As de baixo são representados para representar números inteiros
+    : Long             64 inteiros
+
+    : Int                 32 inteiros
+
+    : Short            16 inteiros
+
+    : Byte              8- até 256 valores        Maximo:127   menor: -128
+
+
+    : Boolean - E true ou false -não tem minimo e maximo - 1 ou 8 não tem minimo e maximo
+
+
+    : String  - Uma cadeia de caracteres, usa aspas duplas “ ”, ela pode crescer normalmente sem limitação de bit-não tem minimo e maximo
+
+
+    :Char =      Usa aspas simples ‘ ’ -não tem minimo e maximo
+
+
+
+
+    var = Variável mutável
+    val = Variável imutável
+
+
+
+    ${ .length} = Para saber o tamanho da variável
+
+
+     fun = palavra reservada que define uma função em kotlin
+
+     A função é algo que posso extrair uma lógica que aparece no meu código, podendo colocar alguma função específica para algo.
+
+    EX: fun calculabonus parametros que ela recebe →(a: int)define o retorno : int ou : Short :Boolean etc {}
+
+    fun calculabonus(a: Int, b: Int, c:Int){
+        println(“O Bonus é: {a + b * c}”)
+        depois chamo o calculaBonus(a, b, c)
+    }
+
+    Para que não seja preciso percorrer todo o codigo procurando essa logica basta alterar a função que ela altera toda logica do codigo.
+*/
+
+    /*
+    println("2 + 2 = ${2 + 2 }")
+    println("2 - 2 = ${2 - 2 }")
+    println("2 /2 = ${2 / 2 }")
+    println("2 * 2 = ${2 * 2 }")
+    println("10 % 4 = ${10 % 4 }")
+    println("2 % 2 = ${2 % 2 }")
+
+    var numero = 10
+
+    numero++ acrescenta +1 no numero
+    println("numero++ = ${numero++}")
+    println("numero-- = ${numero--}")
+
+    ++numero acrecenta +1 antes
+    println("++numero = ${++numero}")
+    println("--numero = ${--numero}")
+
+
+    numero +=2
+
+    para nao precisar colocar numero = numero +2 coloca o +antes do =
+
+    println("numero+= 2 = $numero")
+
+    numero -=2
+    println("numero-= 2 = $numero")
+
+     numero /=2
+    println("numero/= 2 = $numero")
+
+     numero *=2
+    println("numero*= 2 = $numero")
+
+     numero %=3
+    println("numero%= 3 = $numero")
+
+     */
+
+
+    /*
+    Conjunção && o codigo só é executado quandos as 2 expreções são executadas EX:
+    if(expre1 && expre2){
+    codigo aqui
+    } se as 2 for verdade vai executar esse codigo
+     */
+
+    /* Disjunção || o codigo só é executado quando 1 das expreções for executada, somente nao executa quando as 2 forem falsas EX:
+    if(expre1 || expre2){
+    codigo aqui
+    }
+     */
+
+    /* Negação - ! eu nego a expreção se for true vira false se for false vira true
+    if(!expre1){
+    codigo aqui
+    }
+     */
+
+    /*Controle de fluxo if else, operador Elvis, when, for, while, do while,
+    */
+
+
+    /* Tipos compostos
+    if((expre1 && expre2) || expre3 || expre4 ){
+    codigo aqui
+    }
+
+     */
+
+
+    /*OPERADOR Elvis ?:
+//se existe valor em teste use, se não existir, ele for nulo use a outra expressão use 100
+    val test: Int? = null
+    val op: Int =  test ?: 100
+
+
+     */
+
+    /* when
+    Siguinifica quando alguma coisa estiver um valor especifico usamos determinado treicho de codigo
+     */
+
+
+    /*Range intervalo de valores coloca .. ex. 1..99 ele conta todos os numeros dentro desses valores
+    * */
+
+    /* For - loop executa varias vezes ate que algo diga que tem que parar, pois e um loop infinito sem parada,
+
+     */
+
+
+    /*while siguinifica enquanto, enquanto algum valor for verdade voce vai executar um trecho de codigo indicado
+
+
+ */
+
+    /* conversão de dados, sempre ocorre do menor para o maior, pois os maiores nao podem ser convertido para um menor
+    por causa do tamanho deles, ou seja o Byte pode ser convertido para todos os outros mais um double nao pode ser para
+    Byte por ser maior.
+
+    Double 64
+    Float 32
+    long 64
+    Int 32
+    Short 16
+    Byte 8
+     */
+
+
+
+
+    /*Exceções de conversão de dados,
+NumberFormatException é mais completa com valores mais especificos
+Exception é mais simples
+finally
+
+ */
+
+    /* Null Safe
+
+
+ */
+
+    /*pacote cria na pasta src cria como package ele consegue ter responsabilidade arquivos classes dentro dele
+    ele separa para os codigos ficarem com as responsabilidades em pacotes, dividindo entre os pacotes, por que ai
+    fica mais divido e facil de averiguar.
+
+    colocando *  import basic.* importa todos os pacotes da pasta basic
+
+
+     */
+
+    /*ORIENTAÇÃO A OBJETOS
+    / os objetos tem atributos e comportamentos
+    os Atributos na verdade são caracteristicas que marcam o objeto, cor, tamanho, marca, peso, altura, ligado desligado.
+    os comportamentos são oque aquele objeto pode fazer, ex mouse, se for sem fio pode ser desligado ou com fio pode
+    ligar com usb, pode carregar, pode clicar com botao direito, botao esquerdo.
+
+    Os atributos na programação vão virar VARIAVEIS
+    Os comportamentos vão virar     METODOS
+
+    MODULARIDADE Eu posso trabalhar com um objeto independente dos outros objetos
+
+    ENCAPSULAMENTO Detalhes internos de como funciona nao sao conhecidos por outros objetos
+
+    REUSO DE CODIGO um objeto pode ser usado em varias partes do nosso sistema
+
+
+    */
+
+
+
+
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,342 @@
+@file:Suppress("UNREACHABLE_CODE")
+
+import java.lang.NumberFormatException
+
+/*fun calculaBonus(a: Int, b: Int, c: Int): Unit {
+   print("O bonus é: ${a + b + c}")
+
+}*/
+
+/*fun hello(nome: String): String{
+    return " Olá, $nome"
+}
+
+//função de uma unica linha
+fun hello2(nome: String) ="Olá, $nome"
+fun calculaBonus2 (a: Int, b: Int, c: Int) = "O bonus é: ${a + b + c}"
+
+
+fun main() {
+
+    val a: Int= 10
+    val b: Int= 20
+    val c: Int= 30
+    val nome: String = "Célio"
+
+    //println(calculaBonus2(a, b, c ))
+
+   println(hello2("Célio"))
+
+}*/
+
+//if - else
+/*fun maiorIdade(idade: Int) {
+
+
+    if (idade >= 18) {
+        println("Maior de idade")
+    } else {
+        println("Menor de idade")
+    }
+
+
+}
+
+
+fun main() {
+
+    maiorIdade(18)
+    maiorIdade(33)
+    maiorIdade(12)
+}*/
+
+//if - else com retorno Boolean
+/*fun maiorIdade(idade: Int): Boolean {
+    return idade >= 18
+
+}
+
+
+fun main() {
+
+    print(maiorIdade(18))
+    print(maiorIdade(12))
+
+}*/
+//if dentro do else
+/*
+if (cargo == "Coordenador") {
+    return salario * 0.2f
+} else {
+    if (cargo.contains("Gerente")) {
+        if (cargo == "Gerente Senior"){
+            return salario * 2
+        }else {
+            salario * 0.5f
+        }
+    }
+}
+return retorno*/
+
+//if else
+/*
+fun calculaBonus(cargo: String, salario: Float): Float {
+
+    val bonus: Float
+    if (cargo == "Cooernador") {
+        bonus = salario * 0.1f
+    } else if (cargo == "Gerente Junior") {
+        bonus = salario * 0.2f
+    } else {
+        bonus = salario * 2
+    }
+
+    return bonus
+}
+
+fun main() {
+
+    println(calculaBonus("Coordenador", salario = 1000f))
+    println(calculaBonus("Gerente Junior", salario = 2000f))
+    println(calculaBonus("Gerente Senior", salario = 3000f))
+
+}*/
+
+
+//operador ternario não tem no kotlin usa if else
+
+/*fun main() {
+    val valor = 10
+    val a = if (valor == 10) "Sim" else "Não"
+
+}*/
+
+//OPERADOR Elvis ?:
+/*
+fun main(){
+//se existe valor em teste use, se não existir, ele for nulo use a outra expressão use 100
+    val test: Int? = null
+    val op: Int =  test ?: 100
+
+
+}*/
+
+//Operador when e range
+
+/*fun operacao (a: Int, b: Int, c:String): Int{
+
+    when (c){
+        "soma" ->return a + b
+        "Subtração"->{
+            return a - b
+        }
+        else -> {
+            println("Operação Incorreta")
+            return 0
+        }
+    }
+
+    //Range intervalo de valores coloca .. ex. 1..99 ele conta todos os numeros dentro desses valores
+   /* when{
+        a in 1..99 && b > 0 -> {
+            println("Século")
+        }
+    }*/
+
+
+
+}
+fun main(){
+    operacao(a = 10, b = 10, c = "soma")
+    operacao(10,10,"Subtração")
+
+}
+*/
+
+//operador for
+/*função for em varios casos
+fun main(){
+    val nome = "Célio"
+
+    for (a in nome){
+        print("$a ")
+    }
+
+    for (i in 1..100 step 2){
+        print("$i ")
+    }
+
+    for (c in 0..100 step 5){
+        if (c !=80){
+            print("$c ")
+        }
+    }
+
+    for (j in 100 downTo 0){
+        print("$j ")
+    }
+
+
+}*/
+
+//while siguinifica enquanto, executa somente condição verdadeira
+
+
+/*fun main() {
+    //while tem que iniciar e terminar se nao fica em loop infinito
+
+    var a: Int = 0
+    while (a < 100) {
+        println(a)
+        a++
+
+        if (a == 50) {
+            break
+        }
+    }
+
+ */
+
+// do while executa mesmo se a condição for falsa
+/*fun main() {
+    var l: Int = 10
+    do {
+
+    println (l)
+    l += 5
+
+
+    }while (l <= 100)
+    }
+ */
+
+// conversão de valores
+
+/*fun teste(n1: Int){
+
+}
+fun main(){
+    val b1: Byte= 100
+
+    println(b1.toShort())
+    println(b1.toLong())
+    println(b1.toFloat())
+    println(b1.toDouble())
+
+    println("19".toInt())
+    println("19".toFloat())
+    println ("19".toDouble())
+
+
+}*/
+
+// exceções de conversão de valores, sempre as mais especificas primeiro depois as mais simples
+/*fun main() {
+
+    try {
+        println("sdgv".toInt())
+    } catch (e: NumberFormatException){
+        println("Esse valor não é um numero")
+    }
+    catch (e: Exception) {
+        println("Algo de errado ocorreu")
+
+    }finally {
+        println("")
+    }
+
+
+}*/
+
+// Null Safe
+
+/*fun main(){
+    val str: String? = null
+    println(str?.length)
+
+    if (str != null)
+        println(str.length)
+    else{
+        println()
+    }
+}*/
+
+//Pacotes package
+
+//Chamamento de parametros
+/*fun endereco(Rua: String = "", Cidade: String, Estado: String, Cep: String, numero: Int = 0){
+    println("Rua: $Rua")
+    println("Cidade: $Cidade, $Estado, - $Cep")
+}
+//pode colocar em forma desordenada que consegue chamar a função mas o bom seria ordenar
+fun main(){
+        endereco(Cidade= "trindade", Estado = "Goias", Cep = "13")
+
+
+}*/
+
+
+//Parametros vararg
+//sem vararg
+//fun media(n1: Float, n2:Float, n3:Float =0f, n4:Float = 0f){}
+
+/*fun media(vararg notas: Float){
+
+    if (notas.isNotEmpty()){
+        var soma = 0f
+        for (nota in notas) {
+            soma += nota
+        }
+        println("A média é: ${soma/notas.size}")
+    }
+
+}
+fun main(){
+    media(10f, 8f)
+    media(1f,5f,2f,7f)
+
+}*/
+
+//outros parametros vararg
+
+/*fun<T> media(vararg valores: T){
+    for (valor in valores) {
+        println(valor)
+    }
+}
+fun main(){
+    media(1,4f,"",false,'C')
+}*/
+
+//Algumas propriedades e funções
+
+/*fun main(){
+    val str="lorem ipsum"
+    //deixa a primeira letra maiuscula
+    str.capitalize()
+    //Deixa a primeira letra minuscula
+    str.decapitalize()
+    //Verifica se minha string contem algo dentro
+    str.contains("lorem")
+    //Verifica se alguma função começa com alguma letra ou maiuscula ou minuscula
+    str.startsWith("l")
+    //declara uma lista e retorna um tamanho
+    arrayListOf(1,2,3,4,5,6,7,8,9)
+
+
+
+}*/
+
+//ORIENTAÇÃO A OBJETOS / classes
+
+fun main(){
+
+    
+}
+
+
+
+
+
+
+
